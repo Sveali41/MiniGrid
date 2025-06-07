c3e408a85bc7affa318e273ce919f28c6b487559
Siyao <sveali41@gmail.com>
2025-06-07 13:41:29 +0100

    change params

--- modelBased/data/datamodule.py
+++ modelBased/data/datamodule.py
@@ -173,7 +173,7 @@ class WMRLDataModule(pl.LightningDataModule):
             drop_last=True,
             num_workers=self.hparams.n_cpu,
             pin_memory=True,
-            persistent_workers=False
+            persistent_workers=True
         )
 
     def val_dataloader(self):
@@ -184,5 +184,5 @@ class WMRLDataModule(pl.LightningDataModule):
             drop_last=True,
             num_workers=self.hparams.n_cpu,
             pin_memory=True,
-            persistent_workers=False
+            persistent_workers=True
         )
